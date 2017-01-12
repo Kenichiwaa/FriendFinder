79d8aa3849b6b41508b9397c667afd8f0deb7cec
Kenichi <kenichiwa.ninja@gmail.com>
2017-01-12 00:35:46 -0800

    github connection issues push test

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsList = [
+  {
+  name:"Ahmed",
+  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},
+{
+name:"Talia",
+photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores:[
+   3,
+   2,
+   5,
+   3,
+   5,
+   1,
+   2,
+   5,
+   4,
+   1
+ ]
+},
+{
+name:"2Chainz",
+photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores:[
+   5,
+   4,
+   3,
+   2,
+   1,
+   1,
+   2,
+   5,
+   4,
+   1
+ ]
+}
+];
+
+module.exports = friendsList;
